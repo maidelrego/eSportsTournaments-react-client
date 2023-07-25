93441011966cad1912564be05bd83a220082a6f3
Maydel Rego <maidelrego@yahoo.com>
2023-07-25 15:34:37 -0500

    update init

--- src/layout/MainLayout/components/Tournaments/Games.jsx
+++ src/layout/MainLayout/components/Tournaments/Games.jsx
@@ -1,14 +1,15 @@
-import { useEffect, useState } from "react";
-import { doAPIGet } from "../../../../services/api";
+import { useEffect } from "react";
+import { doAPIGet, doAPIPut } from "../../../../services/api";
 import { Fieldset } from "primereact/fieldset";
 import { InputText } from "primereact/inputtext";
 import PropTypes from "prop-types";
 import { Avatar } from "primereact/avatar";
+import { AppSpinner } from "../../../../ui/components/AppSpinner";
+import { useForm } from "../../../../hooks/useForm";
+import { Button } from "primereact/button";
 
 export const Games = (props) => {
-  const [games, setGames] = useState([]);
-  const [score1, setScore1] = useState("0");
-  const [score2, setScore2] = useState("0");
+  const { form, setForm, handleChange } = useForm();
   const teamsIds = props.teams.map((team) => team.id);
   const teams = props.teams;
 
@@ -26,17 +27,37 @@ export const Games = (props) => {
           t.team2 = teams.find((team) => team.id === t.team2);
         }
         console.log(tournamentGames);
-        setGames(tournamentGames);
+        setForm(tournamentGames);
       });
     };
     getGames();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const handleSave = (id) => {
+    const game = form.find((game) => game.id === id);
+    
+    game.team1 = game.team1.id;
+    game.team2 = game.team2.id;
+    game.score1 = parseInt(game.score1);
+    game.score2 = parseInt(game.score2);
+    delete game.id;
+    delete game.logoUrl;
+    delete game.createdAt;
+    
+    console.log(game);
+    
+    doAPIPut(`games/${id}`, game).then((response) => {
+      console.log(response);
+    });
+  }
+
+  console.log('FOOORM', form);
+
   return (
     <>
       <div className="grid mt-5">
-        {games.map((game, index) => (
+        {form.length > 0 ? form.map((game, index) => (
           <div className="col-12 md:col-6" key={game.id}>
             <Fieldset legend={`Fixure ${index + 1}`} className="ma-0 pa-0">
               <div className="flex justify-content-center flex-wrap">
@@ -52,12 +73,14 @@ export const Games = (props) => {
                   <span className="mt-2 text-xs xl:text-lg">
                     ({game.team1.userName})
                   </span>
+                  <Button icon="pi pi-save" severity="success" rounded text onClick={() => handleSave(game.id)} />
                 </div>
                 <InputText
                   type="number"
+                  name="score1"
                   className="p-inputtext-sm mt-5 mb-5 w-2rem xl:w-4rem xl:mt-4 mb-4 text-center xl:text-4xl xl:font-bold"
-                  value={score1}
-                  onChange={(e) => setScore1(e.target.value)}
+                  value={form[index].score1}
+                  onChange={(e) => handleChange(e, index)}
                   autoFocus
                 />
                 <span className="flex align-items-center justify-content-center mr-2 ml-2 xl:font-bold">
@@ -65,9 +88,10 @@ export const Games = (props) => {
                 </span>
                 <InputText
                   type="number"
+                  name="score2"
                   className="p-inputtext-sm mt-5 mb-5 w-2rem xl:w-4rem xl:mt-4 mb-4 text-center xl:text-4xl xl:font-bold"
-                  value={score2}
-                  onChange={(e) => setScore2(e.target.value)}
+                  value={form[index].score2}
+                  onChange={(e) => handleChange(e, index)}
                   autoFocus
                 />
                 <div className="flex flex-column align-items-center justify-content-center ml-3">
@@ -86,7 +110,8 @@ export const Games = (props) => {
               </div>
             </Fieldset>
           </div>
-        ))}
+        )) : <AppSpinner loading={true} />
+        }
       </div>
     </>
   );
