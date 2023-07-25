67522d6147c47f9f2061810a84737e48e9c193ca
Maydel Rego <maidelrego@yahoo.com>
2023-07-25 13:46:07 -0500

    eslint for useEffect

--- src/layout/MainLayout/components/Tournaments/Games.jsx
+++ src/layout/MainLayout/components/Tournaments/Games.jsx
@@ -30,6 +30,7 @@ export const Games = (props) => {
       });
     };
     getGames();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -14,6 +14,7 @@ export const AppRouter = () => {
 
   useEffect(() => {
     startCheckAuthToken();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   if (authStatus === authStatusName.checking) {
